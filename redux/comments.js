7297d77b4a7220a235353b69a05c107b6ef77c72
Brent Bowen <bodunk2k@aol.com>
2020-05-02 14:55:47 -0500

    Week2 Weekend project

--- redux/comments.js
+++ redux/comments.js
@@ -1,6 +1,7 @@
 import * as ActionTypes from './ActionTypes';
 
 export const comments = (state = { errMess: null, comments: []}, action) => {
+    console.log("payload in comments2",action.payload.text);
     switch (action.type) {
         case ActionTypes.ADD_COMMENTS:
             return {...state, errMess: null, comments: action.payload};
@@ -8,6 +9,11 @@ export const comments = (state = { errMess: null, comments: []}, action) => {
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload};
 
+        case ActionTypes.ADD_COMMENT:
+            console.log("payload in comments",action.payload.text);
+            const comment=action.payload;
+            comment.id=state.comments.length;
+            return {...state, comments: state.comments.concat(comment)};
         default:
             return state;
     }
